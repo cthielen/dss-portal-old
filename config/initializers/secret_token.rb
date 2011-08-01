0949cbe72a06241b4738cb064216903ab20e948d
Christopher Thielen <cthielen@gmail.com>
2011-08-01 11:05:18 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DssPortal::Application.config.secret_token = '8ddc67799ab4018cd76514e97ca5eb7c089330ed6e7ff10ffe12c12cb467057bc1ba31879f1dd951ad66ae74ed7b5257e4d85031c7ae1709907b2b8a85330328'
