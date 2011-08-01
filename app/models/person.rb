ceccd15984595b31a89ffa32ddcd219673cec5f8
Christopher Thielen <cthielen@gmail.com>
2011-08-01 16:52:11 -0700

    Added API key support for RM usage.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,5 @@
 class Person < ActiveResource::Base
-  self.site = "http://dss-rm.dev/"
+  self.site = "http://dss-rm.dev/api/"
+  self.user = "DSS Portal"
+  self.password = YAML.load_file("#{Rails.root.to_s}/config/api_key.yml")['keys']['key']
 end
