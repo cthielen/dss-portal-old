34d74663d37be0fb74591466b435805f8ffebbbe
Christopher Thielen <cthielen@gmail.com>
2011-08-10 17:38:05 -0700

    API URI should be configurable.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ActiveResource::Base
-  self.site = "http://dss-rm.dev/api/"
+  self.site = YAML.load_file("#{Rails.root.to_s}/config/api_key.yml")['keys']['uri']
   self.user = "DSS Portal"
   self.password = YAML.load_file("#{Rails.root.to_s}/config/api_key.yml")['keys']['key']
 end
