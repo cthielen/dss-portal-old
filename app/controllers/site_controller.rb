b955aae5318ff534f7d43ff9644ac2239f50f30b
Christopher Thielen <cthielen@gmail.com>
2011-09-27 12:55:49 -0700

    Improvements to app categories.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,6 +3,15 @@ class SiteController < ApplicationController
   
   def index
     @messages = Message.find(current_user.loginid).messages
+    
+    # Determine if the user has any apps directly assigned to them (will generate extra tab)
+    @has_special_apps = false
+    current_user.apps.each do |app|
+      if app.access == "direct"
+        @has_special_apps = true
+        break
+      end
+    end
   end
   
   def logout
